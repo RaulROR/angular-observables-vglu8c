b2a697d095a91b4941fbbe80a859465732ee4474
RaulROR <raulror@gmail.com>
2020-02-16 17:01:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-observables-vglu8c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-observables-vglu8c)
---
+++ src/app/libro-observable.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import{ Libro } from'./libro.model';
+import { LIBROS } from './mocks';
+import{ Observable} from'rxjs/Observable';
+import{ Subscription} from"rxjs/Subscription";
+
+@Injectable()
+export class LibroObservableService {
+  libros: Libro[];
+
+  constructor() { }
+
+  getLibros(): Observable<Libro[]> {
+    return new Observable<Libro[]>( observer => {
+      let libros: Libro[] = [];
+
+      observer.next([]);
+      LIBROS.forEach((libro, index) => {
+        setTimeout(() => {
+          libros.push(libro);
+          console.log('Enviando libro desde observable....');
+          observer.next(libros);
+        }, (index + 1) * 1500);
+      });
+
+      setTimeout(() => {
+        observer.complete();
+      }, (LIBROS.length + 1) * 1500);
+
+    });
+  }
+
+}
